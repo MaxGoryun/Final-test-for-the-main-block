3139598df9b9c1c353a15ddd73f7576ea4c2ed58
MaxGoryun <khorn667@gmail.com>
2023-09-21 13:36:18 +0300

    My first commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите длину массива: ");
+int LenArr = Convert.ToInt32(Console.ReadLine());
+
+string[] MyArray = new string[LenArr];
+
+for (int i = 0; i < MyArray.Length; i++)
+{
+    Console.Write($"Введите элемент массива под индексом {i}");
+    MyArray[i] = Console.ReadLine()!;
+}
+
+
+for (int j = 0; j < MyArray.GetLength(0); j++)
+{
+    Console.Write(MyArray[j] + " , ");
+}
+
